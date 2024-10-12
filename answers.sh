0682e9e712ba0e06df25d13984f6c936e511a57b
stevenkwoon <stevenkwoon@tamu.edu>
2024-10-11 20:29:51 -0500

    Creating Table

--- answers.sh
+++ answers.sh
@@ -7,7 +7,7 @@ UPDATE Students SET Points = 500 WHERE Name='Basma';
 UPDATE Students SET Points = 100 WHERE Name='Alex';
 
 CREATE TABLE graduates(
-    ID INTEGER  PRIMARY KEY AUTOINCREMENT,
+    ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     Name  TEXT NOT NULL UNIQUE,
     Age INTEGER,
 	Gender TEXT,
@@ -17,7 +17,7 @@ CREATE TABLE graduates(
 
 INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
 UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
-DELETE FROM graduates WHERE Name='Layal';
+DELETE FROM students WHERE Name='Layal';
 
 SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
 SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
